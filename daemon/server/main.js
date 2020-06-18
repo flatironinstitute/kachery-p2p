b77b476f870d3928f0102a977cad95adfade1cdb
Jeremy Magland <jmagland@flatironinstitute.org>
2020-06-18 14:24:31 -0400

    testing download

--- daemon/server/main.js
+++ daemon/server/main.js
@@ -6,6 +6,8 @@ import JsonSocket from 'json-socket';
 import ram from 'random-access-memory';
 import deepExtend from 'deep-extend';
 import { exec } from 'child_process';
+import util from 'util';
+import { resolve } from 'path';
 
 const daemon_dir = process.env['KACHERY_P2P_DAEMON_DIR'] || undefined;
 if (!daemon_dir) {
@@ -15,13 +17,16 @@ if (!daemon_dir) {
 
 const main = async () => {
     const swarmConnectionManager = new SwarmConnectionManager();
-    await swarmConnectionManager.joinSwarm('test1');
+    await swarmConnectionManager.joinSwarm('test5');
 
     const fileSeeker = new FileSeeker(swarmConnectionManager);
     const fileProvider = new FileProvider(swarmConnectionManager);
 
-    const info = await fileSeeker.getFileInfoForSha1('b4f1725eff03cddf9625e491e4863c926b03126a');
+    const info = await fileSeeker.getFileInfoForSha1('dbf95a050023d48aa8d7c9b8bd63d36ee79ffeca');
     console.log('INFO:', info);
+
+    await fileSeeker.downloadFileForSha1('dbf95a050023d48aa8d7c9b8bd63d36ee79ffeca');
+    console.log('Done.');
 }
 
 class FileSeeker {
@@ -29,26 +34,59 @@ class FileSeeker {
         this._swarmConnectionManager = swarmConnectionManager;
         this._start();
     }
-    async getFileInfoForSha1(sha1) {
-        // Set state.seekingFileInfos[sha1] = true on all swarms
+    async downloadFileForSha1(sha1) {
+        // Set state.seekingFileContent[sha1] = true on all swarms
         for (let swarmName of this._swarmConnectionManager.swarmNames()) {
             const swarmConnection = this._swarmConnectionManager.swarmConnection(swarmName);
             const s = swarmConnection.getState();
             swarmConnection.updateState({
-                seekingFileInfos: {
+                seekingFileContent: {
+                    [sha1]: true
+                }
+            })
+        }
+        let includedPeerIds = {};
+        while (true) {
+            const fileDownloader = new FileDownloader(sha1);
+            for (let swarmName of this._swarmConnectionManager.swarmNames()) {
+                const swarmConnection = this._swarmConnectionManager.swarmConnection(swarmName);
+                for (let id of swarmConnection.peerIds()) {
+                    if (!(id in includedPeerIds)) {
+                        const peerConnection = swarmConnection.peerConnection(id);
+                        if (sha1 in peerConnection._receivedFileContentOffers) {
+                            includedPeerIds[id] = true;
+                            fileDownloader.addPeerConnection(peerConnection);
+                        }
+                    }
+                }
+            }
+            if (fileDownloader.finished()) {
+                break;
+            }
+            await sleepMsec(1000);
+        }
+        this._removeSeekingFileContentInSwarms(this._swarmConnectionManager.swarmNames(), sha1);
+    }
+    async getFileInfoForSha1(sha1) {
+        // Set state.seekingFileInfo[sha1] = true on all swarms
+        for (let swarmName of this._swarmConnectionManager.swarmNames()) {
+            const swarmConnection = this._swarmConnectionManager.swarmConnection(swarmName);
+            const s = swarmConnection.getState();
+            swarmConnection.updateState({
+                seekingFileInfo: {
                     [sha1]: true
                 }
             })
         }
         while (true) {
-            // Check for state.fileInfos[sha1] on all peer connections of all swarms
             for (let swarmName of this._swarmConnectionManager.swarmNames()) {
                 const swarmConnection = this._swarmConnectionManager.swarmConnection(swarmName);
                 for (let id of swarmConnection.peerIds()) {
                     const peerConnection = swarmConnection.peerConnection(id);
                     if (sha1 in peerConnection._receivedFileInfos) {
-                        // Delete state.seekingFileInfos[sha1] = true on all swarms
+                        // Delete state.seekingFileInfo[sha1] = true on all swarms
                         this._removeSeekingFileInfoInSwarms(this._swarmConnectionManager.swarmNames(), sha1);
+                        // TODO: check for consistency if more than one received, from different peers
                         return peerConnection._receivedFileInfos[sha1];
                     }
                 }
@@ -61,13 +99,58 @@ class FileSeeker {
             const swarmConnection = this._swarmConnectionManager.swarmConnection(swarmName);
             const s = swarmConnection.getState();
             swarmConnection.updateState({
-                seekingFileInfos: {
+                seekingFileInfo: {
+                    [sha1]: null
+                }
+            })
+        }
+    }
+    _removeSeekingFileContentInSwarms(swarmNames, sha1) {
+        for (let swarmName of this._swarmConnectionManager.swarmNames()) {
+            const swarmConnection = this._swarmConnectionManager.swarmConnection(swarmName);
+            const s = swarmConnection.getState();
+            swarmConnection.updateState({
+                seekingFileContent: {
                     [sha1]: null
                 }
             })
         }
     }
     async _start() {
+    }
+}
+
+class FileDownloader {
+    constructor(sha1) {
+        this._sha1 = sha1;
+        this._peerConnections = [];
+        this._finished = false;
+        this._start();
+    }
+    addPeerConnection(c) {
+        this._peerConnections.push(c);
+    }
+    finished() {
+        return this._finished;
+    }
+    async _start() {
+        while (true) {
+            if (this._peerConnections.length > 0) {
+                const c = this._peerConnections[0];
+                const requestId = randomString(10);
+                c.sendMessage({
+                    name: 'requestFileContent',
+                    sha1: this._sha1,
+                    requestId: requestId
+                });
+                const msg = await c.waitForMessageWithRequestId(requestId);
+                const buf = Buffer.from(msg.data_b64, 'base64');
+                console.log('length of buffer', buf.length);
+                this._finished = true;
+                return;
+            }
+            await sleepMsec(100);
+        }
     }
 }
 
@@ -86,25 +169,24 @@ class FileProvider {
         // Loop through the swarms
         for (let swarmName of this._swarmConnectionManager.swarmNames()) {
             const swarmConnection = this._swarmConnectionManager.swarmConnection(swarmName);
-            // Find all sha1 for which state.seekingFileInfos[sha1] is true on at least one peer
-            const beingSaught = {};
+            // Find all sha1 for which state.seekingFileInfo[sha1] is true on at least one peer
+            const infoBeingSaught = {};
             for (let id of swarmConnection.peerIds()) {
                 const peerConnection = swarmConnection.peerConnection(id);
                 const ps = peerConnection.getPeerState();
-                for (let sha1 in (ps.seekingFileInfos || {})) {
-                    beingSaught[sha1] = true;
+                for (let sha1 in (ps.seekingFileInfo || {})) {
+                    infoBeingSaught[sha1] = true;
                 }
             }
             // For each file info being saught, let's see if we can provide that info
-            let updateFileInfos = {};
-            for (let sha1 in beingSaught) {
-                const info = await this._loadFileInfo(sha1);
+            for (let sha1 in infoBeingSaught) {
+                const info = await _loadFileInfo(sha1);
                 if (info) {
                     for (let id of swarmConnection.peerIds()) {
                         const peerConnection = swarmConnection.peerConnection(id);
                         if (peerConnection) {
                             const ps = peerConnection.getPeerState();
-                            for (let sha1 in (ps.seekingFileInfos || {})) {
+                            for (let sha1 in (ps.seekingFileInfo || {})) {
                                 peerConnection.sendMessage({
                                     name: 'fileInfo',
                                     sha1: sha1,
@@ -115,26 +197,75 @@ class FileProvider {
                     }
                 }
             }
+
+            // Find all sha1 for which state.seekingFileContent[sha1] is true on at least one peer
+            const contentBeingSaught = {};
+            for (let id of swarmConnection.peerIds()) {
+                const peerConnection = swarmConnection.peerConnection(id);
+                const ps = peerConnection.getPeerState();
+                for (let sha1 in (ps.seekingFileContent || {})) {
+                    contentBeingSaught[sha1] = true;
+                }
+            }
+            // For each file content being saught, let's see if we can provide that info
+            for (let sha1 in contentBeingSaught) {
+                const info = await _loadFileInfo(sha1);
+                if (info) {
+                    for (let id of swarmConnection.peerIds()) {
+                        const peerConnection = swarmConnection.peerConnection(id);
+                        if (peerConnection) {
+                            const ps = peerConnection.getPeerState();
+                            for (let sha1 in (ps.seekingFileContent || {})) {
+                                peerConnection.sendMessage({
+                                    name: 'fileContentOffer',
+                                    sha1: sha1,
+                                    info: info
+                                })
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
-    async _loadFileInfo(sha1) {
-        let json;
-        try {
-            json = await executeAndGetStdout(`kachery-info sha1://${sha1}`);
-        }
-        catch(err) {
-            return null;
-        }
-        let obj;
-        try {
-            obj = JSON.parse(json);
-        }
-        catch(err) {
-            return null;
-        }
-        delete obj['path'];
-        return obj;
+}
+
+const _loadFileInfo = async (sha1) => {
+    let json;
+    try {
+        json = await executeAndGetStdout(`kachery-info sha1://${sha1}`);
     }
+    catch(err) {
+        return null;
+    }
+    let obj;
+    try {
+        obj = JSON.parse(json);
+    }
+    catch(err) {
+        return null;
+    }
+    delete obj['path'];
+    return obj;
+}
+
+const _loadFileContent = async (sha1) => {
+    let json;
+    try {
+        json = await executeAndGetStdout(`kachery-info sha1://${sha1}`);
+    }
+    catch(err) {
+        return null;
+    }
+    let obj;
+    try {
+        obj = JSON.parse(json);
+    }
+    catch(err) {
+        return null;
+    }
+    const readFile = util.promisify(fs.readFile);
+    return await readFile(obj.path);
 }
 
 class SwarmConnectionManager {
@@ -159,7 +290,8 @@ class SwarmConnection {
         this.id = randomString(10);
         this.swarmName = swarmName;
         this._state = {
-            seekingFileInfos: {},
+            seekingFileInfo: {},
+            seekingFileContent: {},
             seekingFiles: {},
             filesAvailableForDownload: {}
         }
@@ -294,6 +426,8 @@ class PeerConnection {
         this._peerState = {};
         this._state = {};
         this._receivedFileInfos = {};
+        this._receivedFileContentOffers = {};
+        this._messageReceivedCallbacks = {};
     }
     setIncomingSocket(jsonSocket) {
         this._incomingJsonSocket = jsonSocket;
@@ -325,7 +459,7 @@ class PeerConnection {
         })
         this._outgoingJsonSocket.sendMessage({name: 'ready'});
     }
-    _handleMessage(msg) {
+    async _handleMessage(msg) {
         if (msg.name === 'updateState') {
             this._peerState = deepExtendAndDeleteNull(this._peerState, msg.update);
         }
@@ -334,6 +468,24 @@ class PeerConnection {
         }
         else if (msg.name === 'fileInfo') {
             this._receivedFileInfos[msg.sha1] = msg.info;
+        }
+        else if (msg.name === 'fileContentOffer') {
+            this._receivedFileContentOffers[msg.sha1] = {info: msg.info};
+        }
+        else if (msg.name == 'requestFileContent') {
+            const x = await _loadFileContent(msg.sha1);
+            if (x) {
+                this.sendMessage({
+                    name: 'fileContent',
+                    data_b64: x.toString('base64'),
+                    requestId: msg.requestId
+                })
+            }
+        }
+        else if (msg.requestId in this._messageReceivedCallbacks) {
+            for (let cb of this._messageReceivedCallbacks[msg.requestId]) {
+                cb(msg);
+            }
         }
     }
     sendMessage(msg) {
@@ -359,6 +511,17 @@ class PeerConnection {
             socket.sendMessage(msg);
         }
         asyncHelper();
+    }
+    async waitForMessageWithRequestId(requestId) {
+        return new Promise((resolve, reject) => {
+            const cb = (msg) => {
+                resolve(msg);
+            }
+            if (!this._messageReceivedCallbacks[requestId]) {
+                this._messageReceivedCallbacks[requestId] = [];
+            }
+            this._messageReceivedCallbacks[requestId].push(cb);
+        });
     }
     updateState(update) {
         this._state = deepExtendAndDeleteNull(this._state, update);
