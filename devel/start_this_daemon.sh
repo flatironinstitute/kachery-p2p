603969a973f97d7c546dfd87b5a62591df3ce2e3
Jeremy Magland <jeremy.magland@gmail.com>
2020-06-26 21:29:29 -0400

    updates

--- devel/start_this_daemon.sh
+++ devel/start_this_daemon.sh
@@ -4,4 +4,4 @@ DIRECTORY=$(cd `dirname $0` && pwd)
 export KACHERY_STORAGE_DIR=$DIRECTORY/kachery-storage
 mkdir -p $KACHERY_STORAGE_DIR
 
-$DIRECTORY/../daemon/bin/kachery-p2p-daemon start --swarm test1
+$DIRECTORY/../daemon/bin/kachery-p2p-daemon start --swarm test5
