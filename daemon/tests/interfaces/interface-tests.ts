2f48e8a86ea3d71fe04601861b0612613fb23e5c
Jeff Soules <jsoules@flatironinstitute.org>
2020-10-01 15:33:35 -0400

    Install mocha, chai. Add example test file/directory.

---
+++ daemon/tests/interfaces/interface-tests.ts
@@ -0,0 +1,129 @@
+import * as mocha from 'mocha'; // import types for mocha e.g. describe
+import { expect } from 'chai'; // test library
+
+import * as ut from '../../src/interfaces/core';
+
+// Example objects (test fixtures)
+
+
+describe('Basic typeguards', () => {
+    describe('JSON Handling', () => {
+        // Test fixture for JSON object handling
+        const exampleJSONObject = {example: ['json', {object: 1}, '---']}
+
+        it('JSON object successfully recognized', () => {
+            expect(ut.isJSONObject(exampleJSONObject)).to.be.true;
+        });
+        it('JSON object recognized as serializable', () => {
+            expect(ut.isJSONSerializable(exampleJSONObject)).to.be.true;
+        });
+        it('JSON object parsed successfully', () => {
+            const obj = ut.tryParseJsonObject(JSON.stringify(exampleJSONObject));
+            expect(obj).to.deep.equal(exampleJSONObject);
+        });
+        it('TryParse returns null without throwing on parse failure', () => {
+            expect(ut.tryParseJsonObject('This is not json')).to.be.null;
+        });
+    });
+    describe('Object handling', () => {
+        it('isObject() returns true on valid object input', () => {
+            expect(ut.isObject({name: 'Object', type: 'Object', value: 5})).to.be.true;
+        });
+        it('isObject() returns false on non-object input', () => {
+            expect(ut.isObject('I am a string')).to.be.false;
+        })
+    });
+    describe('String handling', () => {
+        it('isString() returns true on string input', () => {
+            expect(ut.isString("5")).to.be.true;
+        });
+        it('isString() returns false on non-string input', () => {
+            expect(ut.isString(5)).to.be.false;
+        });
+    })
+    describe('Function handling', () => {
+        it('isFunction() returns true on function input', () => {
+            expect(ut.isFunction(() => 5)).to.be.true;
+        });
+        it('isFunction() returns false on non-function input', () => {
+            expect(ut.isFunction(5)).to.be.false;
+        });
+    });
+    describe('Number handling', () => {
+        it('isNumber() returns true on number input', () => {
+            expect(ut.isNumber(5.432)).to.be.true;
+            expect(ut.isNumber(-783)).to.be.true;
+        });
+        it('isNumber() returns false on non-numeric input', () => {
+            expect(ut.isNumber('5')).to.be.false;
+            expect(ut.isNumber(BigInt(5))).to.be.false;
+        });
+        it('isBigInt() returns true on bigint input', () => {
+            expect(ut.isBigInt(BigInt(5))).to.be.true;
+        })
+        it('isBigInt() returns false on non-bigint input', () => {
+            expect(ut.isBigInt(5)).to.be.false;
+        })
+    });
+    describe('Null handling', () => {
+        it('isNull() returns true on null input', () => {
+            expect(ut.isNull(null)).to.be.true;
+        });
+        it('isNull() returns false on non-null input', () => {
+            expect(ut.isNull(5)).to.be.false;
+        });
+    });
+    describe('Boolean handling', () => {
+        it('isBoolean() returns true on boolean input', () => {
+            expect(ut.isBoolean(true)).to.be.true;
+            expect(ut.isBoolean(false)).to.be.true;
+            expect(ut.isBoolean(5 === 5)).to.be.true;
+            expect(ut.isBoolean('a' === 'a')).to.be.true;
+        });
+        it('isBoolean() returns false on non-boolean input', () => {
+            expect(ut.isBoolean('false')).to.be.false;
+        });
+    })
+})
+
+describe('Utility comparisons', () => {
+    const checker = ut.isOneOf([ut.isBoolean, ut.isString]);
+    describe('Type union', () => {
+        it('isOneOf() recognizes all enumerated types', () => {
+            expect(checker('I am a string')).to.be.true;
+            expect(checker(false)).to.be.true;
+        });
+        it('isOneOf() returns false on non-included types', () => {
+            expect(checker(5)).to.be.false;
+        });
+    });
+    describe('Optional parameters', () => {
+        // TODO
+    });
+    describe('Equality check', () => {
+        const isMyString = ut.isEqualTo('myString');
+        it('isEqualTo() returns true on equality', () => {
+            expect(isMyString('myString')).to.be.true;
+        });
+        it('isEqualTo() returns false on non-equality', () => {
+            expect(isMyString('not my string')).to.be.false;
+        });
+    });
+    describe('Array check', () => {
+        const areStrings = ut.isArrayOf(ut.isString);
+        it('isArrayOf() returns true on array of specified type', () => {
+            expect(areStrings(['These', 'are', 'four', 'strings'])).to.be.true;
+        });
+        it('isArrayOf() returns false on non-array', () => {
+            expect(areStrings('This is not an array')).to.be.false;
+        });
+        it('isArrayOf() returns false on wrongly-typed members', () => {
+            expect(areStrings(['These', 'are', 'not', 5, 'strings'])).to.be.false;
+        });
+    });
+    describe('Object validation to spec', () => {
+        // TODO -- see notes in core.ts.
+        it('_validateObject() returns false on non-object input')
+    });
+
+})
