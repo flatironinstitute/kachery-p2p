b76e2bf8d2980eed7589192e78074faf215498ec
Jeremy Magland <jmagland@flatironinstitute.org>
2020-10-21 14:56:47 -0400

    developing new protocol

---
+++ daemon/src/nodeToNodeRequestHandlers/handleStartStreamViaUdpRequest.ts
@@ -0,0 +1,30 @@
+import { errorMessage, NodeId } from '../interfaces/core'
+import { StartStreamViaUdpRequestData, StartStreamViaUdpResponseData } from '../interfaces/NodeToNodeRequest'
+import KacheryP2PNode from '../KacheryP2PNode'
+
+export const handleStartStreamViaUdpRequest = async (node: KacheryP2PNode, fromNodeId: NodeId, requestData: StartStreamViaUdpRequestData): Promise<StartStreamViaUdpResponseData> => {
+    let { streamId } = requestData
+    const rn = node.remoteNodeManager().getRemoteNode(fromNodeId)
+    if (!rn) {
+        return {
+            requestType: 'startStreamViaUdp',
+            success: false,
+            errorMessage: errorMessage('Unable to find remote node')
+        }
+    }
+    try {
+        await rn.startStreamViaUdpToRemoteNode(streamId)
+    }
+    catch(err) {
+        return {
+            requestType: 'startStreamViaUdp',
+            success: false,
+            errorMessage: errorMessage(err.message)
+        }
+    }
+    return {
+        requestType: 'startStreamViaUdp',
+        success: true,
+        errorMessage: null
+    }
+}
