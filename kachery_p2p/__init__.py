da9372c08786b1966a1d43d23d9f02e78be5654c
Jeremy Magland <jmagland@flatironinstitute.org>
2020-10-30 16:09:17 -0400

    incr version

--- kachery_p2p/__init__.py
+++ kachery_p2p/__init__.py
@@ -1,12 +1,10 @@
-__version__ = "0.4.28"
+__version__ = "0.5.0"
 
-from .core import get_node_id
-from .core import get_channels
-from .core import find_file
-from .core import load_file, load_text, load_object, load_npy, load_bytes, read_dir
-from .core import store_file, store_text, store_object, store_npy
-from .core import start_daemon, stop_daemon
-from .feeds import create_feed, load_feed, load_subfeed, get_feed_id, watch_for_new_messages
-from .exceptions import LoadFileError
 from ._testdaemon import TestDaemon
-from .core import _experimental_config
+from .core import (_experimental_config, find_file, get_channels, get_node_id,
+                   load_bytes, load_file, load_npy, load_object, load_text,
+                   read_dir, start_daemon, stop_daemon, store_file, store_npy,
+                   store_object, store_text)
+from .exceptions import LoadFileError
+from .feeds import (create_feed, get_feed_id, load_feed, load_subfeed,
+                    watch_for_new_messages)
