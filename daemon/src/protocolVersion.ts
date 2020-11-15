219ebae41af92c64f23a51a8ea5a506cec20dfd9
Jeremy Magland <jmagland@flatironinstitute.org>
2020-11-14 22:49:36 -0500

    increment version

--- daemon/src/protocolVersion.ts
+++ daemon/src/protocolVersion.ts
@@ -1,7 +1,7 @@
 import { DaemonVersion, ProtocolVersion } from './interfaces/core';
 
-const PROTOCOL_VERSION = 'kachery-p2p-0.5.6';
-const DAEMON_VERSION = 'kachery-p2p-0.5.6';
+const PROTOCOL_VERSION = 'kachery-p2p-0.5.7';
+const DAEMON_VERSION = 'kachery-p2p-0.5.7';
 
 export const protocolVersion = (): ProtocolVersion => {
     return PROTOCOL_VERSION as any as ProtocolVersion;
